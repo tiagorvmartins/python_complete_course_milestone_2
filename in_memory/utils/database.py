d48769e43856d7c9f25e2dee64e2487cba65df4c
Tiago Martins <tiagorvmartins@gmail.com>
2018-12-10 00:59:36 +0000

    Final commit of section 7

---
+++ in_memory/utils/database.py
@@ -0,0 +1,24 @@
+"""
+Concerned with storing and retrieving books from a list.
+"""
+
+books = []
+
+def add_book(name, author):
+    books.append({'name': name, 'author': author, 'read': False })
+
+def get_all_books():
+    return books
+
+def mark_book_as_read(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+            break
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name]
+
+
+
